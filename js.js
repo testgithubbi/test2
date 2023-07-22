5917c930a00cf1bfcdab6f8ac91cc26b986ca760
testgithubbi <123469328+testgithubbi@users.noreply.github.com>
2023-07-22 21:28:23 +0530

    test-2

---
+++ js.js
@@ -0,0 +1,206 @@
+var para1 = document.querySelector(".box2").style;
+	var para2 = document.querySelector(".box3").style;
+	var para3 = document.querySelector(".box4").style;
+	var para4 = document.querySelector(".box5").style;
+	var para5 = document.querySelector(".box6").style;
+	var para6 = document.querySelector(".box7").style;
+	var para7 = document.querySelector(".box8").style;
+	var para8 = document.querySelector(".box9").style;
+
+	function box1() {
+		para1.order="9";
+		para2.order="2";
+		para3.order="3";
+		para4.order="4";
+		para5.order="5";
+		para6.order="6";
+		para7.order="7";
+		para8.order="8";
+	}
+
+	function box2() {
+		para1.order="2";
+		para2.order="3";
+		para3.order="4";
+		para4.order="5";
+		para5.order="6";
+		para6.order="7";
+		para7.order="8";
+		para8.order="9";
+	}
+
+	function box3() {
+		para1.order="9";
+		para2.order="2";
+		para3.order="3";
+		para4.order="4";
+		para5.order="5";
+		para6.order="6";
+		para7.order="7";
+		para8.order="8";
+	}
+
+	function box4() {
+		para1.order="2";
+		para2.order="3";
+		para3.order="4";
+		para4.order="5";
+		para5.order="6";
+		para6.order="7";
+		para7.order="8";
+		para8.order="9";
+	}
+
+	function box5() {
+		para1.order="9";
+		para2.order="2";
+		para3.order="3";
+		para4.order="4";
+		para5.order="5";
+		para6.order="6";
+		para7.order="7";
+		para8.order="8";
+	}
+
+	function box6() {
+		para1.order="2";
+		para2.order="3";
+		para3.order="4";
+		para4.order="5";
+		para5.order="6";
+		para6.order="7";
+		para7.order="8";
+		para8.order="9";
+	}
+
+	function box7() {
+		para1.order="9";
+		para2.order="2";
+		para3.order="3";
+		para4.order="4";
+		para5.order="5";
+		para6.order="6";
+		para7.order="7";
+		para8.order="8";
+	}
+
+	function box8() {
+		para1.order="2";
+		para2.order="3";
+		para3.order="4";
+		para4.order="5";
+		para5.order="6";
+		para6.order="7";
+		para7.order="8";
+		para8.order="9";
+	}
+
+	function box9() {
+		para1.order="9";
+		para2.order="2";
+		para3.order="3";
+		para4.order="4";
+		para5.order="5";
+		para6.order="6";
+		para7.order="7";
+		para8.order="8";
+	}
+
+	function box10() {
+		para1.order="2";
+		para2.order="3";
+		para3.order="4";
+		para4.order="5";
+		para5.order="6";
+		para6.order="7";
+		para7.order="8";
+		para8.order="9";
+	}
+
+	function box11() {
+		para1.order="9";
+		para2.order="2";
+		para3.order="3";
+		para4.order="4";
+		para5.order="5";
+		para6.order="6";
+		para7.order="7";
+		para8.order="8";
+	}
+
+	function box12() {
+		para1.order="2";
+		para2.order="3";
+		para3.order="4";
+		para4.order="5";
+		para5.order="6";
+		para6.order="7";
+		para7.order="8";
+		para8.order="9";
+	}
+
+	function box13() {
+		para1.order="9";
+		para2.order="2";
+		para3.order="3";
+		para4.order="4";
+		para5.order="5";
+		para6.order="6";
+		para7.order="7";
+		para8.order="8";
+	}
+
+	function box14() {
+		para1.order="2";
+		para2.order="3";
+		para3.order="4";
+		para4.order="5";
+		para5.order="6";
+		para6.order="7";
+		para7.order="8";
+		para8.order="9";
+	}
+
+	function box15() {
+		para1.order="9";
+		para2.order="2";
+		para3.order="3";
+		para4.order="4";
+		para5.order="5";
+		para6.order="6";
+		para7.order="7";
+		para8.order="8";
+	}
+
+	function box16() {
+		para1.order="2";
+		para2.order="3";
+		para3.order="4";
+		para4.order="5";
+		para5.order="6";
+		para6.order="7";
+		para7.order="8";
+		para8.order="9";
+	}
+
+	function box17() {
+		para1.order="9";
+		para2.order="2";
+		para3.order="3";
+		para4.order="4";
+		para5.order="5";
+		para6.order="6";
+		para7.order="7";
+		para8.order="8";
+	}
+
+	function box18() {
+		para1.order="2";
+		para2.order="3";
+		para3.order="4";
+		para4.order="5";
+		para5.order="6";
+		para6.order="7";
+		para7.order="8";
+		para8.order="9";
+	}
